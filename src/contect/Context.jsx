46f7cac424e39c4b05c08520845e43b1f56bce18
sejal <user.email: sejalkumari5679@gmail.com>
2025-09-21 15:57:16 +0530

    api call

---
+++ src/contect/Context.jsx
@@ -0,0 +1,26 @@
+import { createContext } from "react";
+import runChat from "../config/Gemini";
+
+export const Context = createContext()
+
+const ContextProvider = (props) =>{
+    
+    const onSent = async(prompt) =>{
+    await runChat(prompt)
+    }
+
+    onSent('what is react js')
+
+    const contextValue ={
+
+
+    }
+
+    return(
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    )
+}
+
+export default ContextProvider
